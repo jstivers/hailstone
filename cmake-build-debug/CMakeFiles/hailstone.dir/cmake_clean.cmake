6806e4d59d0c66fabe4ae0203bda498053471b2b
John Stivers <jstivers@usc.edu>
2017-01-24 17:26:52 -0800

    you are cancer

---
+++ cmake-build-debug/CMakeFiles/hailstone.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hailstone.dir/main.cpp.o"
+  "hailstone.pdb"
+  "hailstone"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hailstone.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
