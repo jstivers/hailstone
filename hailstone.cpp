bc8f97f63bdbb908e8beb56c2b37b39ccc09fe29
John Stivers <jstivers@usc.edu>
2017-01-24 17:54:06 -0800

    poo

--- hailstone.cpp
+++ hailstone.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    //variables
+    int input;
+    //io
+    cout<<("Enter a number: ");
+    cin>>input;
+    //algorithm
+    if(input % 2 == 0){
+        input = input /2;
+        cout<<input;
+    }else{
+        input = (input * 3)+1;
+        cout<<input;
+    }
     return 0;
 }
